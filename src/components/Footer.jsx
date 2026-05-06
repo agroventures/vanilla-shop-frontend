c001e70f1027b35f516c960156d98f33ebbf3d3a
Dilnuwan Weerasuriya <dilnuwanweerasuriya@gmail.com>
2026-05-06 10:58:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { 
-//   Facebook, 
-//   Instagram, 
-//   Twitter, 
   Mail, 
   Phone, 
   MapPin,
@@ -36,17 +33,16 @@ const Footer = () => {
     {
       href: 'https://www.facebook.com/thevanillashopsl',
       label: 'Facebook',
-      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>,
-    },
+      icon: "fa-facebook-f"    },
     {
       href: 'https://www.instagram.com/the_vanillashop',
       label: 'Instagram',
-      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"/><path fill="none" stroke="white" strokeWidth="2" d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line fill="none" stroke="white" strokeWidth="2" x1="17.5" y1="6.5" x2="17.51" y2="6.5"/></svg>,
+      icon: "fa-instagram"
     },
     {
       href: 'https://www.tiktok.com/@the_vanillashop',
       label: 'TikTok',
-      icon: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24"><path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.18 8.18 0 0 0 4.78 1.52V6.75a4.85 4.85 0 0 1-1.01-.06z"/></svg>,
+      icon: "fa-tiktok"
     },
   ];
 
@@ -204,7 +200,7 @@ const Footer = () => {
                   className="w-10 h-10 bg-white/10 hover:bg-vanilla-600 rounded-full flex items-center justify-center transition-colors"
                   aria-label={social.label}
                 >
-                  {social.icon}
+                  <i className={`fa-brands ${social.icon}`}></i>
                 </motion.a>
               ))}
             </div>
